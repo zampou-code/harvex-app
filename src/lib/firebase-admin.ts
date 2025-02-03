aad7079f280e22ca603985dbbbffca39ff692b87
zampou-code <zampou.elec@gmail.com>
2025-02-03 09:50:13 +0000

    Debug

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -22,7 +22,7 @@ export const storage = getStorage(app);
 export const db = initFirestore({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, "\n"),
   }),
 });
