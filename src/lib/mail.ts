7124b2597fc8b1eb35bf605820cd8ad833d62135
zampou-code <zampou.elec@gmail.com>
2025-02-14 13:43:44 +0000

    Update - resolve send mail - 13

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -11,9 +11,8 @@ type sendMailType = {
 export async function sendMail({ to, subject, body }: sendMailType) {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 587,
-    // port: 465,
-    secure: true,
+    port: 587, // Ou 465 si besoin
+    secure: false, // false pour 587, true pour 465
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
@@ -23,13 +22,17 @@ export async function sendMail({ to, subject, body }: sendMailType) {
   const emailHtml = await render(body);
 
   try {
-    await transporter.verify();
-    const options = {
+    const options: nodemailer.SendMailOptions = {
       to,
       subject,
       html: emailHtml,
-      from: '"Harvex Groupe" <no-reply@harvexgroupe.com>',
-    } satisfies nodemailer.SendMailOptions;
+
+      from: `"Harvex Groupe" ${process.env.SMTP_USER}`, // Doit être un email valide LWS
+      // '"Harvex Groupe" <no-reply@harvexgroupe.com>',
+      // from: process.env.SMTP_USER, // Doit être un email valide LWS
+    };
+
+    console.log("SMTP config:", transporter.options);
     console.log("email data: ", { ...options, html: "" });
 
     await transporter.sendMail(options);
