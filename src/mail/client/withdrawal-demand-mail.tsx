9e29f36ea78322504aeee59d4eae73f67dc23017
zampou-code <zampou.elec@gmail.com>
2025-02-16 16:47:12 +0000

    Finish update

--- src/mail/client/withdrawal-demand-mail.tsx
+++ src/mail/client/withdrawal-demand-mail.tsx
@@ -47,7 +47,7 @@ export function WithdrawalDemandMail({
         📲 Pour toute vérification ou assistance, contactez-nous via WhatsApp :
       </Text>
       <Text className="leading-relaxed text-gray-700 mb-5">
-        🇨🇮 Côte d'Ivoire : +225 07 13 06 59 59 / +225 07 18 10 10 78
+        🇨🇮 Côte d&apos;Ivoire : +225 07 13 06 59 59 / +225 07 18 10 10 78
       </Text>
       <Text className="leading-relaxed text-gray-700 mb-5">
         🇲🇱 Mali : +223 70 62 56 84
